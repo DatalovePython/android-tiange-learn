eb0ad5a07b0285d6b8580f3c5cc8ca497f3a674e
luzhongchuan <wyhyy20161121@163.com>
2020-07-08 15:01:23 +0800

    1：imageview 加载网络图片无法加载bug修复(通过本地引入)

--- app/src/main/java/com/newland/tiange/ImageViewActivity.java
+++ app/src/main/java/com/newland/tiange/ImageViewActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 //引入外部包后报错
-//import com.bumptech.glide.Glide;
+import com.bumptech.glide.Glide;
 
 public class ImageViewActivity extends AppCompatActivity {
 
@@ -18,7 +18,7 @@ public class ImageViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_image_view);
         mIv4 = (ImageView) findViewById(R.id.iv_4);
 
-//        Glide.with(this).load("https://www.baidu.com/img/dong_54209c0ff3da32eecc31f340c08a18f6.gif").into(mIv4);
+        Glide.with(this).load("https://www.baidu.com/img/dong_54209c0ff3da32eecc31f340c08a18f6.gif").into(mIv4);
 
 
     }
