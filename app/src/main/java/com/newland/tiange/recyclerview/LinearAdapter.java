2120b5d09648fdd0f12039f63b2755da6987e0d9
luzhongchuan <wyhyy20161121@163.com>
2020-07-10 17:26:24 +0800

    1：recyclerview 文字视图和图片视图拼接

--- app/src/main/java/com/newland/tiange/recyclerview/LinearAdapter.java
+++ app/src/main/java/com/newland/tiange/recyclerview/LinearAdapter.java
@@ -4,8 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,7 +14,7 @@ import com.newland.tiange.R;
 
 import java.util.List;
 
-public class LinearAdapter extends RecyclerView.Adapter<LinearAdapter.LinearViewHolder> {
+public class LinearAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private Context mContext;
     private OnItemClickListener mListener;
@@ -29,22 +29,47 @@ public class LinearAdapter extends RecyclerView.Adapter<LinearAdapter.LinearView
 
     @NonNull
     @Override
-    public LinearAdapter.LinearViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new LinearViewHolder(LayoutInflater.from(mContext).inflate(R.layout.layout_linear_item,parent,false));
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if(viewType == 0){
+            return new LinearViewHolder(LayoutInflater.from(mContext).inflate(R.layout.layout_linear_item,parent,false));
+        }else{
+            return new LinearViewHolder2(LayoutInflater.from(mContext).inflate(R.layout.layout_linear_item_2,parent,false));
+        }
+
     }
 
     @Override
-    public void onBindViewHolder(@NonNull LinearAdapter.LinearViewHolder holder, final int position) {
-        holder.textView.setText("hello  world");
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //方法一
+    public int getItemViewType(int position) {
+        if(position %2 == 0){
+            return 0;
+        }else {
+            return 1;
+        }
+//        return super.getItemViewType(position);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+        if(getItemViewType(position)==0){
+            ((LinearViewHolder)holder).textView.setText("hello  world");
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //方法一
 //                Toast.makeText(mContext,"click...."+position,Toast.LENGTH_SHORT).show();
-                //方法二
-                mListener.onclick(position);
-            }
-        });
+                    //方法二
+                    mListener.onclick(position);
+                }
+            });
+        }else{
+//            ((LinearViewHolder2)holder).imageView.setImageResource(R.drawable.xinyuanjieyi);
+            ((LinearViewHolder2)holder).textView.setText("hello  luzhongchuan");
+
+
+        }
+
+
+
 
     }
 
@@ -60,6 +85,20 @@ public class LinearAdapter extends RecyclerView.Adapter<LinearAdapter.LinearView
         public LinearViewHolder(@NonNull View itemView) {
             super(itemView);
             textView = (TextView)itemView.findViewById(R.id.tv_title);
+        }
+    }
+
+    class LinearViewHolder2 extends RecyclerView.ViewHolder
+    {
+
+        private TextView textView;
+        private ImageView imageView;
+
+        public LinearViewHolder2(@NonNull View itemView)
+        {
+            super(itemView);
+            textView = itemView.findViewById(R.id.tv_title);
+            imageView = itemView.findViewById(R.id.iv);
         }
     }
 
