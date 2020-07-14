0d72ee98ee6e3fe1f3d2c93eb3c3559e9340666f
luzhongchuan <wyhyy20161121@163.com>
2020-07-14 13:49:39 +0800

    1：Dialog 五种样式的展示

--- app/src/main/java/com/newland/tiange/DialogActivity.java
+++ app/src/main/java/com/newland/tiange/DialogActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.newland.tiange.util.ToastUtil;
 
@@ -16,6 +18,7 @@ public class DialogActivity extends AppCompatActivity {
     private Button mBtnDialog2;
     private Button mBtnDialog3;
     private Button mBtnDialog4;
+    private Button mBtnDialog5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +28,13 @@ public class DialogActivity extends AppCompatActivity {
         mBtnDialog2 = (Button) findViewById(R.id.btn_dialog2);
         mBtnDialog3 = (Button) findViewById(R.id.btn_dialog3);
         mBtnDialog4 = (Button) findViewById(R.id.btn_dialog4);
+        mBtnDialog5 = (Button) findViewById(R.id.btn_dialog5);
         Onclick onclick = new Onclick();
         mBtnDialog1.setOnClickListener(onclick);
         mBtnDialog2.setOnClickListener(onclick);
         mBtnDialog3.setOnClickListener(onclick);
         mBtnDialog4.setOnClickListener(onclick);
+        mBtnDialog5.setOnClickListener(onclick);
 
 
     }
@@ -59,10 +64,72 @@ public class DialogActivity extends AppCompatActivity {
                     }).show();
                     break;
                 case R.id.btn_dialog2:
+                    final String[] array2 = new String[]{"男", "女"};
+                    AlertDialog.Builder builder2 = new AlertDialog.Builder(DialogActivity.this);
+                    builder2.setTitle("请选择性别").setItems(array2, new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i)
+                        {
+                            ToastUtil.showMsg(DialogActivity.this, array2[i]);
+                        }
+                    }).show();
                     break;
                 case R.id.btn_dialog3:
+                    final String[] array3 = new String[]{"男", "女"};
+                    AlertDialog.Builder builder3 = new AlertDialog.Builder(DialogActivity.this);
+                    builder3.setTitle("请选择性别").setSingleChoiceItems(array3, 0, new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i)
+                        {
+                            ToastUtil.showMsg(DialogActivity.this, array3[i]);
+                            dialogInterface.dismiss();
+                        }
+                    }).setCancelable(false).show();
                     break;
                 case R.id.btn_dialog4:
+                    final String[] array4 = new String[]{"唱歌", "跳舞", "写代码"};
+                    final boolean[] isCheckd = new boolean[]{false, false, true};
+                    AlertDialog.Builder builder4 = new AlertDialog.Builder(DialogActivity.this);
+                    builder4.setTitle("选择兴趣").setMultiChoiceItems(array4, isCheckd, new DialogInterface.OnMultiChoiceClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i, boolean b)
+                        {
+                            ToastUtil.showMsg(DialogActivity.this, array4[i] + ": " + isCheckd[i]);
+                        }
+                    }).setPositiveButton("确定", new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i)
+                        {
+                            //回调函数
+                        }
+                    }).setNegativeButton("取消", new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i)
+                        {
+
+                        }
+                    }).show();
+                    break;
+                case R.id.btn_dialog5:
+                    AlertDialog.Builder builder5 = new AlertDialog.Builder(DialogActivity.this);
+                    View loginView = LayoutInflater.from(DialogActivity.this).inflate(R.layout.layout_dialog, null);
+                    EditText username = loginView.findViewById(R.id.et_username);
+                    EditText password = loginView.findViewById(R.id.et_password);
+                    Button btnLogin = loginView.findViewById(R.id.btn_login);
+                    btnLogin.setOnClickListener(new View.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(View view)
+                        {
+                            //
+                        }
+                    });
+                    builder5.setTitle("请先登录").setView(loginView).show();
                     break;
 
             }
