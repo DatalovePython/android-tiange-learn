9846e9aa21034dc039257886bc8bf10c84434b27
luzhongchuan <wyhyy20161121@163.com>
2020-07-15 19:39:47 +0800

    1：activity 启动模式（部分）

--- app/src/main/java/com/newland/tiange/jump/AActivity.java
+++ app/src/main/java/com/newland/tiange/jump/AActivity.java
@@ -4,7 +4,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,23 +18,41 @@ import com.newland.tiange.UIActivity;
 public class AActivity extends AppCompatActivity {
 
     private Button mBtnJump;
+    private Button mBtnJump2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
+        Log.d("AActivity", "----onCreate----");
+        Log.d("AActivity", "taskId" + getTaskId() + " ,hash" + hashCode());
+        try {
+            logTaskName();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
         mBtnJump = (Button) findViewById(R.id.btn_jump);
+
         mBtnJump.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(AActivity.this, BActivity.class);
                 //演示A页面的数据传递给B页面
                 Bundle bundle = new Bundle();
-                bundle.putString("name","卢中川");
-                bundle.putInt("age",18);
+                bundle.putString("name", "卢中川");
+                bundle.putInt("age", 18);
                 intent.putExtras(bundle);
-//                startActivity(intent);
-                startActivityForResult(intent,0);
+                startActivity(intent);
+//                startActivityForResult(intent,0);
+            }
+        });
+        mBtnJump2 = (Button) findViewById(R.id.btn_jump2);
+        mBtnJump2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AActivity.this, AActivity.class);
+                startActivity(intent);
+
             }
         });
     }
@@ -40,6 +61,24 @@ public class AActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Toast.makeText(AActivity.this, data.getExtras().getString("title"),Toast.LENGTH_LONG).show();
+        Toast.makeText(AActivity.this, data.getExtras().getString("title"), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d("AActivity", "----onCreate----");
+        Log.d("AActivity", "taskId" + getTaskId() + " ,hash" + hashCode());
+        try {
+            logTaskName();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void logTaskName() throws PackageManager.NameNotFoundException {
+        ActivityInfo activityInfo = getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
+        Log.d("AActivity", activityInfo.taskAffinity);
     }
 }
--- app/src/main/java/com/newland/tiange/jump/BActivity.java
+++ app/src/main/java/com/newland/tiange/jump/BActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,6 +23,13 @@ public class BActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+        Log.d("BActivity", "----onCreate----");
+        Log.d("BActivity", "taskId" + getTaskId() + " ,hash" + hashCode());
+        try {
+            logTaskName();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
         final Bundle bundle = getIntent().getExtras();
         String name = bundle.getString("name");
         int age = bundle.getInt("age");
@@ -39,5 +49,22 @@ public class BActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Log.d("BActivity", "----onCreate----");
+        Log.d("BActivity", "taskId" + getTaskId() + " ,hash" + hashCode());
+        try {
+            logTaskName();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        super.onNewIntent(intent);
+    }
+
+    private void logTaskName() throws PackageManager.NameNotFoundException {
+        ActivityInfo activityInfo = getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
+        Log.d("BActivity", activityInfo.taskAffinity);
     }
 }
